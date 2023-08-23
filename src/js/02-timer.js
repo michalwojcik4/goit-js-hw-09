28169c4214698dc7c59ecb4a4f0fb19b16757ee6
michalwojcik4 <mwjcik4@gmail.com>
2023-08-23 23:22:02 +0200

    flatpickr

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,2 +1,18 @@
+import flatpickr from "flatpickr";
+import "flatpickr/dist/flatpickr.min.css";
 
-  
+const datetimePicker = document.querySelector('#datetime-picker');
+
+flatpickr(datetimePicker, options);
+
+const options = {
+    enableTime: true,
+    time_24hr: true,
+    defaultDate: new Date(),
+    minuteIncrement: 1,
+    onClose(selectedDates) {
+      console.log(selectedDates[0]);
+    },
+  };
+
+  console.log(datetimePicker)
